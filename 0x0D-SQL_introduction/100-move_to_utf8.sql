0cdd1728a43f3b7652a4274c884ee7ac8e0235ab
Marvel paul <djmarvel@yahoo.com>
2024-02-15 11:29:10 -0800

    sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
 -- Change character SET and COLLATE
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
+-- cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p
+USE hbtn_0c_0
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
