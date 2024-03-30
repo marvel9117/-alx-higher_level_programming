bd71fc066584581de91d46e13a7351c4c73b24fd
Marvel paul <djmarvel@yahoo.com>
2024-03-30 03:46:48 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+
+with urlib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+    content = resp.read()
+    print("Body response:")
+    print("/t -type: {}".format(type(content)))
+    print("/t -content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode('utf-8')))
